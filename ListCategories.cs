e9ba16ab420e7ee24fe414ac2e9c3b71f410ff61
vthejaj <vishnutheja@hotmail.com>
2020-03-30 18:40:30 +0530

    Add project files.

---
+++ ListCategories.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDMDemo
+{
+    public class ListCategories
+    {
+        static void Main(string[] args)
+        {
+            msdbEntities1 ctx = new msdbEntities1();
+            // ctx.Database.Log = Console.WriteLine;
+
+            foreach(var cat in ctx.categories)
+            {
+                Console.WriteLine("****" + cat.CatDesc +  "*******");
+                // Get all products for category cat 
+                foreach(var p in cat.products)
+                {
+                    Console.WriteLine(p.ProdName);
+                }
+
+            }
+
+        }
+    }
+}
---
+++ ListProducts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDMDemo
+{
+    public class ListProducts
+    {
+        static void Main(string[] args)
+        {
+            msdbEntities1 ctx = new msdbEntities1();
+            ctx.Database.Log = Console.WriteLine;
+
+            // Eager load - bring all from products and categories 
+            foreach(product p in ctx.products.Include("category"))
+            {
+                Console.WriteLine( $"{p.ProdName}  - {p.category.CatDesc}");
+            }
+        }
+    }
+}
